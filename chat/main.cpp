328aebcb1435a7b53687c0849390671e0df5c930
MarS-37 <master_sbo@mail.ru>
2023-07-22 18:47:00 +0300

    begin code

---
+++ chat/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	cout << "Welcome to the chat!" << endl;
+	cout << "To output help, type /help" << endl;
+
+	return 0;
+}
