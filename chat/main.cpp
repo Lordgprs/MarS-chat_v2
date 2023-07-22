fcecb6cabf821ae4a8a816966a06479faee84c8a
MarS-37 <master_sbo@mail.ru>
2023-07-22 19:11:46 +0300

    main func

--- chat/main.cpp
+++ chat/main.cpp
@@ -4,9 +4,27 @@
 using namespace std;
 
 int main()
-{
+{	
 	cout << "Welcome to the chat!" << endl;
 	cout << "To output help, type /help" << endl;
+
+	// хранит текст введеный пользователем
+	string input_text;
+
+	while (true)
+	{
+		getline(cin, input_text);
+
+		try {
+			// отработка алгоритма программы
+		}
+		catch (const exception e) {
+			// отработка исключений
+		}
+
+	}
+
+
 
 	return 0;
 }
