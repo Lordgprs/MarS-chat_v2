ec15acc33de9ca84baa8dc2c833ab5af2e835107
MarS-37 <master_sbo@mail.ru>
2023-07-22 19:40:13 +0300

    command /help

--- chat/main.cpp
+++ chat/main.cpp
@@ -3,6 +3,18 @@
 
 using namespace std;
 
+void displayHelp() {
+	std::cout << "Available commands:" << std::endl;
+	std::cout << "   /help - chat help, displays a list of commands to manage the chat" << std::endl;
+	std::cout << "   /signup - registration, user enters data for registration" << std::endl;
+	std::cout << "   /signin - authorization, only a registered user can authorize" << std::endl;
+	std::cout << "   /logout - user logout" << std::endl;
+	std::cout << "   /remove - delete registered user" << std::endl;
+	std::cout << "   /exit - close the program" << std::endl;
+	std::cout << "   @username is an addressable message," << std::endl;
+	std::cout << "     not an addressable message for all users.\n" << std::endl;
+}
+
 int main()
 {
 	cout << "Welcome to the chat!" << endl;
@@ -16,7 +28,10 @@ int main()
 		cout << "> ";
 		getline(cin, input_text);
 
-		if (input_text == "/exit") {
+		if (input_text == "/help") {
+			displayHelp();
+		}
+		else if (input_text == "/exit") {
 			// closing the program
 			break;
 		}
